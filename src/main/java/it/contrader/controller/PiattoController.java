f976f01e0126786be7acbe181ed228cb818ae030
SAMUEL-PC\Samuel Sirca <samuelsirca@gmail.com>
2020-05-25 15:13:18 +0200

    fix piatto

--- src/main/java/it/contrader/controller/PiattoController.java
+++ src/main/java/it/contrader/controller/PiattoController.java
@@ -1,20 +1,15 @@
 package it.contrader.controller;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import it.contrader.dto.ClienteDTO;
+import org.springframework.web.bind.annotation.RestController;
 import it.contrader.dto.PiattoDTO;
 import it.contrader.service.PiattoService;
 
-@Controller
+@RestController
 @RequestMapping("/piatto")
+@CrossOrigin(origins = "http://localhost:4200")
 public class PiattoController extends AbstractController <PiattoDTO> {
 
 	@Autowired
